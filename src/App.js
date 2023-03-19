7933828a13b30ceacde87f7078a7dddec625036e
Jay <pirozhekagmail.comgit config --global user.name Jay@>
2023-03-19 22:14:56 +0500

    finished navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>GPT-3</h1>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
