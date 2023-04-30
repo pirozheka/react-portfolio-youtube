fe28a37fbfcbf8544d3624c7b9e6347848804e42
Jay <pirozhekagmail.comgit config --global user.name Jay@>
2023-04-30 22:00:55 +0500

    finished the first section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -33,11 +33,11 @@ const Navbar = () => {
           {toggleMenu && (
             <div className="gpt3__navbar-menu_container scale-up-center">
               <div className="gpt3__navbar-menu_container-links">
-              <p><a href='#home'>Home</a></p>
-          <p><a href='#wgpt3'>What is GPT3?</a></p>
-          <p><a href='#possibility'>Open AI</a></p>
-          <p><a href='#features'>Case studies</a></p>
-          <p><a href='#blog'>Library</a></p>
+                <p><a href='#home'>Home</a></p>
+                <p><a href='#wgpt3'>What is GPT3?</a></p>
+                <p><a href='#possibility'>Open AI</a></p>
+                <p><a href='#features'>Case studies</a></p>
+                <p><a href='#blog'>Library</a></p>
              </div>
           <div className="gpt3__navbar-menu_container-links-sign">
                <p>Sign in</p>
